283f35a5748cbd7494811a9c1225cc7572df9d7e
Ellis Gibbons <egibbons256@gmail.com>
2025-12-10 14:17:12 -0500

    Implement time synchronization test

--- bsafe/sleep.hpp
+++ bsafe/sleep.hpp
@@ -10,60 +10,78 @@ const uint32_t gdo2 = D1;
 CC1101 radio = new Module(cs, gdo0, RADIOLIB_NC, gdo2);
 
 RTC_DATA_ATTR unsigned long long last_heartbeat_us = 0;
-const unsigned long long HEARTBEAT_INTERVAL_US = 5 * 1000 * 1000;
+RTC_DATA_ATTR unsigned long long epoch = 0;
+// RTC_DATA_ATTR bool first_boot = true;
+const unsigned long long CYCLE_TIME_US = 7 * 1000 * 1000;
+const unsigned long long WAKE_TIME_US = 250 * 1000;
+const unsigned long long SLEEP_TIME_US = CYCLE_TIME_US - WAKE_TIME_US;
+
+volatile bool available = false;
 
 void setup() {
     Serial.begin(115200);
     delay(2000);
-    Serial.println("Woke up!");
+    Serial.printf("Woke up, epoch = %lld\n", epoch);
 
     radio.begin();
+
+    // while (true) {
+    //     Serial.printf("Receiving: ");
+    //     uint8_t buf[15];
+    //     int err = radio.receive(buf, 15);
+    //     buf[14] = 0;
+    //     // radio.finishReceive();
+    //     // radio.startReceive();
+    //     if (buf[0] == 1) {
+    //         // Sync packet
+    //         epoch = *(unsigned long long*)&buf[1];
+    //         Serial.printf("Syncing, epoch = %lld\n", epoch);
+    //     } else {
+    //         // Data packet
+    //         Serial.printf("Received '%s', err = %d\n", &buf[1], err);
+    //     }
+    // }
+    //
+
+    radio.setPacketReceivedAction([] {
+        available = true;
+    });
+
     radio.startReceive();
 
-    switch (esp_sleep_get_wakeup_cause()) {
-        case ESP_SLEEP_WAKEUP_TIMER:
-            Serial.println("Woke up from timer");
-            break;
-        case ESP_SLEEP_WAKEUP_VBAT_UNDER_VOLT:
-            Serial.println("Woke up from low battery voltage");
-            break;
-        case ESP_SLEEP_WAKEUP_EXT0:
-            Serial.println("Woke up from ext0");
-            break;
-        case ESP_SLEEP_WAKEUP_EXT1:
-            Serial.println("Woke up from ext1");
-            // Handle radio
-            if (1) {
-                // "Hello, world!\0"
-                uint8_t buf[14];
-                int err = radio.readData(buf, 14);
-                buf[13] = 0;
-                radio.finishReceive();
-                Serial.printf("Received '%s', err = %d\n", buf, err);
-                radio.startReceive();
+    unsigned long long last_wake_us = esp_rtc_get_time_us();
+    Serial.printf("Sending heartbeat at %lld, offset = %lld\n", last_wake_us, (last_wake_us - epoch) % CYCLE_TIME_US);
+
+    while (esp_rtc_get_time_us() < (last_wake_us + WAKE_TIME_US)) {
+        if (available) {
+            available = false;
+
+            // "Hello, world!\0"
+            const size_t len = 15;
+            uint8_t buf[len];
+            int err = radio.readData(buf, len);
+            buf[len - 1] = 0;
+            radio.finishReceive();
+            radio.startReceive();
+            if (buf[0] == 1) {
+                // Sync packet
+                epoch = *(unsigned long long*)&buf[1];
+                Serial.printf("Syncing, epoch = %lld\n", epoch);
             } else {
-                Serial.println("Nothing to receive");
+                // Data packet
+                Serial.printf("Received '%s', err = %d\n", &buf[1], err);
             }
-            break;
-        case ESP_SLEEP_WAKEUP_TOUCHPAD:
-            Serial.println("Woke up from touchpad");
-            break;
-        default:
-            Serial.println("Woke up from other event");
-            break;
+        }
     }
 
-    if (esp_rtc_get_time_us() > (last_heartbeat_us + HEARTBEAT_INTERVAL_US)) {
-        last_heartbeat_us = esp_rtc_get_time_us();
-        Serial.printf("Sending heartbeat at %lld\n", last_heartbeat_us);
-    } else {
-        Serial.printf("Not sending heartbeat yet, time = %lld, next heartbeat at %lld\n", esp_rtc_get_time_us(), last_heartbeat_us + HEARTBEAT_INTERVAL_US);
+    radio.sleep();
+    unsigned long long offset = (esp_rtc_get_time_us() - epoch) % CYCLE_TIME_US;
+    if (epoch == 0) {
+        epoch = esp_rtc_get_time_us();
+        offset = 0;
     }
-
-    esp_sleep_enable_timer_wakeup(HEARTBEAT_INTERVAL_US);
-    esp_sleep_enable_ext1_wakeup(1 << gdo0 | 1 << gdo2, ESP_EXT1_WAKEUP_ANY_HIGH);
-
-    Serial.println("Going to sleep");
+    esp_sleep_enable_timer_wakeup(CYCLE_TIME_US - offset);
+    Serial.printf("Going to sleep, measured offset of %lld, sleeping for %lld\n", offset, CYCLE_TIME_US - offset);
     esp_deep_sleep_start();
 }
 
---
+++ bsafe/test.hpp
@@ -0,0 +1,40 @@
+#include "esp_rtc_time.h"
+#include <Arduino.h>
+#include <RadioLib.h>
+
+const uint32_t cs = D6;
+const uint32_t gdo0 = D5;
+const uint32_t gdo2 = D4;
+CC1101 radio = new Module(cs, gdo0, RADIOLIB_NC, gdo2);
+
+const unsigned long long CYCLE_TIME_US = (5 + 2) * 1000 * 1000;
+const unsigned long long WAKE_TIME_US = 250 * 1000;
+unsigned long long epoch;
+
+void setup() {
+    Serial.begin(115200);
+    delay(2000);
+
+    radio.begin();
+}
+
+void loop() {
+    if (esp_rtc_get_time_us() > (epoch + CYCLE_TIME_US)) {
+        epoch += CYCLE_TIME_US;
+
+        // Send sync packet
+        const size_t len = 15; // For hello world message
+        uint8_t buf[len];
+        buf[0] = 1;
+        *(unsigned long long*)&buf[1] = epoch;
+        buf[14] = 0;
+        int err = radio.transmit(buf, len);
+        Serial.printf("Sent sync, epoch = %lld, err = %d\n", epoch, err);
+
+        // Send as many data packets as possible
+        while (esp_rtc_get_time_us() < (epoch + WAKE_TIME_US)) {
+            err = radio.transmit((uint8_t*)"\0Hello, world!", len);
+            Serial.printf("Sent data, err = %d\n", err);
+        }
+    }
+}
